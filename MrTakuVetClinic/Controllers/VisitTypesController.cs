0c6da098ac5bcc1a61dcfe596b2d4a39ad47fc65
isaiahsymoens <ibsymoens.work@gmail.com>
2024-07-25 15:37:22 +0800

    add view and view types contollers

---
+++ MrTakuVetClinic/Controllers/VisitTypesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MrTakuVetClinic.Data;
+using MrTakuVetClinic.Entities;
+using System.Threading.Tasks;
+
+namespace MrTakuVetClinic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public VisitTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _context.VisitsTypes.ToListAsync() });
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PetType> GetVisitTypeById(int id)
+        {
+            var petType = _context.VisitsTypes.FindAsync(id);
+            if (petType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(petType);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostVisitType(VisitType visitType)
+        {
+            _context.VisitsTypes.Add(visitType);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ MrTakuVetClinic/Controllers/VisitsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MrTakuVetClinic.Data;
+using MrTakuVetClinic.Entities;
+using System.Threading.Tasks;
+
+namespace MrTakuVetClinic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VisitsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _context.Visits.ToListAsync() });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Visit>> GetVisitById(int id)
+        {
+            var visit = await _context.Visits
+                .Include(v => v.VisitType)
+                .FirstOrDefaultAsync(v => v.VisitId == id);
+
+            if (visit == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(visit);
+        }
+
+        [HttpPost]
+        public IActionResult PostVisit([FromBody] Visit visit)
+        {
+            if (visit == null)
+            {
+                return BadRequest("Pet data is required");
+            }
+
+            var user = _context.Pets.Find(visit.PetId);
+            if (user == null)
+            {
+                ModelState.AddModelError("PetId", "Invalid PetId");
+                return BadRequest(ModelState);
+            }
+
+            _context.Visits.Add(visit);
+            _context.SaveChanges();
+
+            return Ok(new { Message = "Successfully added." });
+        }
+    }
+}
