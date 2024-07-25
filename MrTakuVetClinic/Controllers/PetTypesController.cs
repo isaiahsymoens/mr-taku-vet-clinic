9e1c70f619b14095a1313f2a0608928040efb2e4
isaiahsymoens <ibsymoens.work@gmail.com>
2024-07-25 13:02:49 +0800

    add lookup tables controllers

---
+++ MrTakuVetClinic/Controllers/PetTypesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MrTakuVetClinic.Data;
+using MrTakuVetClinic.Entities;
+using System.Threading.Tasks;
+
+namespace MrTakuVetClinic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public PetTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _context.PetTypes.ToListAsync() });
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PetType> GetPetTypeById(int id)
+        {
+            var petType = _context.PetTypes.FindAsync(id);
+            if (petType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(petType);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostPetType(PetType petType)
+        {
+            _context.PetTypes.Add(petType);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ MrTakuVetClinic/Controllers/UserTypesController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MrTakuVetClinic.Data;
+using MrTakuVetClinic.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace MrTakuVetClinic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public UserTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _context.UserTypes.ToListAsync() });
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<UserType> GetUserTypeById(int id)
+        {
+            var userType = _context.UserTypes.FindAsync(id);
+            if (userType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userType);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostUserType(UserType userType)
+        {
+            _context.UserTypes.Add(userType);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
