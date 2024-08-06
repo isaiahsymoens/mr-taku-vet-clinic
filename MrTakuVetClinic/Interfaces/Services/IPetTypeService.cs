b5b54109b812c5a45cf38bee1f895f0db284a178
isaiahsymoens <ibsymoens.work@gmail.com>
2024-08-06 08:09:34 +0800

    wip - pet type service interface

---
+++ MrTakuVetClinic/Interfaces/Services/IPetTypeService.cs
@@ -0,0 +1,13 @@
+﻿using MrTakuVetClinic.Entities;
+using MrTakuVetClinic.Models;
+using System.Threading.Tasks;
+
+namespace MrTakuVetClinic.Interfaces.Services
+{
+    public interface IPetTypeService
+    {
+        Task<ApiResponse<PetType>> GetAllPetTypesAsync();
+        Task<ApiResponse<PetType>> GetPetTypeByIdAsync(int id);
+        Task<ApiResponse<PetType>> PostPetTypeAsync(PetType petType);
+    }
+}
