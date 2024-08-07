8718bac941c179cab24a6456a466fe6f0601722a
isaiahsymoens <ibsymoens.work@gmail.com>
2024-08-07 13:08:52 +0800

    wip - lookup tables automapper

---
+++ MrTakuVetClinic/Mappings/PetTypeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MrTakuVetClinic.DTOs.PetType;
+using MrTakuVetClinic.Entities;
+
+namespace MrTakuVetClinic.Mappings
+{
+    public class PetTypeProfile : Profile
+    {
+        public PetTypeProfile()
+        {
+            CreateMap<PetType, PetTypeDto>();
+            CreateMap<PetTypePostDto, PetType>();
+            CreateMap<PetTypeDto, PetType>();
+        }
+    }
+}
---
+++ MrTakuVetClinic/Mappings/UserTypeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MrTakuVetClinic.DTOs.UserType;
+using MrTakuVetClinic.Entities;
+
+namespace MrTakuVetClinic.Mappings
+{
+    public class UserTypeProfile : Profile
+    {
+        public UserTypeProfile()
+        {
+            CreateMap<UserType, UserTypeDto>();
+            CreateMap<UserTypePostDto, UserType>();
+            CreateMap<UserTypeDto, UserType>();
+        }
+    }
+}
