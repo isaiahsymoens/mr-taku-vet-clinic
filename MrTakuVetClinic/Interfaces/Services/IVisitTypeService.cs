af9bfcdec2ad10416c9c91e784209e4b1c793cb3
isaiahsymoens <ibsymoens.work@gmail.com>
2024-08-07 13:42:24 +0800

    wip - visit type

---
+++ MrTakuVetClinic/Interfaces/Services/IVisitTypeService.cs
@@ -0,0 +1,14 @@
+﻿using MrTakuVetClinic.DTOs.VisitType;
+using MrTakuVetClinic.Entities;
+using MrTakuVetClinic.Models;
+using System.Threading.Tasks;
+
+namespace MrTakuVetClinic.Interfaces.Services
+{
+    public interface IVisitTypeService
+    {
+        Task<ApiResponse<VisitTypeDto>> GetAllVisitTypesAsync();
+        Task<ApiResponse<VisitTypeDto>> GetVisitTypeByIdAsync(int id);
+        Task<ApiResponse<VisitType>> PostVisitTypeAsync(VisitType visitType);
+    }
+}
--- MrTakuVetClinic/Services/VisitTypeService.cs
+++ MrTakuVetClinic/Services/VisitTypeService.cs
@@ -1,36 +1,54 @@
-﻿using MrTakuVetClinic.Entities;
+﻿using AutoMapper;
+using FluentValidation;
+using MrTakuVetClinic.DTOs.VisitType;
+using MrTakuVetClinic.Entities;
 using MrTakuVetClinic.Helpers;
 using MrTakuVetClinic.Interfaces.Repositories;
 using MrTakuVetClinic.Interfaces.Services;
 using MrTakuVetClinic.Models;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MrTakuVetClinic.Services
 {
-    public class VisitTypeService : IVisitTytpeService
+    public class VisitTypeService : IVisitTypeService
     {
         private readonly IVisitTypeRepository _visitTypeRepository;
+        private readonly IValidator<VisitTypePostDto> _visitTypeValidator;
+        private readonly IMapper _mapper;
 
-        public VisitTypeService(IVisitTypeRepository visitTypeRepository)
+        public VisitTypeService(
+            IVisitTypeRepository visitTypeRepository,
+            IValidator<VisitTypePostDto> visitTypeValidator,
+            IMapper mapper)
         {
             _visitTypeRepository = visitTypeRepository;
+            _visitTypeValidator = visitTypeValidator;
+            _mapper = mapper;
         }
 
-        public async Task<ApiResponse<VisitType>> GetAllVisitTypesAsync()
+        public async Task<ApiResponse<VisitTypeDto>> GetAllVisitTypesAsync()
         {
             return ApiResponseHelper
-                .SuccessResponse<VisitType>(200, await _visitTypeRepository.GetAllAsync());
+                .SuccessResponse<VisitTypeDto>(
+                    200, 
+                    (await _visitTypeRepository.GetAllAsync())
+                    .Select(v => _mapper.Map<VisitTypeDto>(v))
+                );
         }
 
-        public async Task<ApiResponse<VisitType>> GetVisitTypeByIdAsync(int id)
+        public async Task<ApiResponse<VisitTypeDto>> GetVisitTypeByIdAsync(int id)
         {
             var visitType = await _visitTypeRepository.GetByIdAsync(id);
             if (visitType == null)
             {
-                return ApiResponseHelper.FailResponse<VisitType>(404, new { Message = "Visit type does not exist." });
+                return ApiResponseHelper.FailResponse<VisitTypeDto>(
+                    404, 
+                    new { Message = "Visit type does not exist." }
+                );
             }
             return ApiResponseHelper
-                .SuccessResponse<VisitType>(200, visitType);
+                .SuccessResponse<VisitTypeDto>(200, _mapper.Map<VisitTypeDto>(visitType));
         }
 
         public async Task<ApiResponse<VisitType>> PostVisitTypeAsync(VisitType visitType)
